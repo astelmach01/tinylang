d139fb0368aa9081883ffa84bb42443b2ee63c43
astelmach01 <astelmach20@gmail.com>
2023-09-20 22:39:31 -0700

    fix setup

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="tinylang",
     version="0.1",
     packages=find_packages(),
-    description="A simple langchain alternative",
+    description="A tiny language interpreter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
