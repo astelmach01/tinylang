040737df9896d6a85fe5bf0a9021152f965c6071
astelmach01 <astelmach20@gmail.com>
2023-09-20 22:50:32 -0700

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="tinylang",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     description="A tiny language interpreter",
     long_description=long_description,
