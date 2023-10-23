de97098a6569af3b666d9bd5992eff7fc695cbbe
astelmach01 <astelmach20@gmail.com>
2023-10-22 17:04:51 -0700

    Bump version: 0.6.1 → 0.6.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-version = "0.6.1"
+version = "0.6.2"
 
 setup(
     name="tinylang",
