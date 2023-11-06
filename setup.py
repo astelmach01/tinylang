f79564a99425955a9c09e54439fcd9bd23e2767f
astelmach01 <astelmach20@gmail.com>
2023-11-06 13:10:51 -0800

    Bump version: 1.1.0 → 2.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
-version = "1.1.0"
+version = "2.0.0"
 
 setup(
     name="tinylang",
